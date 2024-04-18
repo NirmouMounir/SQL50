a3c6255b1b1e16514bf17f33a68846c5eed3d3f6
NirmouMounir <mounir.abakhour@gmail.com>
2024-04-18 12:56:12 +0200

    link to problem in comment

--- easy/pb595.sql
+++ easy/pb595.sql
@@ -1,3 +1,3 @@
--- https://leetcode.com/problems/big-countries/?envType=study-plan-v2&envId=top-sql-50
+-- https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
 
 select name, population, area from World where area >= 3000 and population >= 25000000;
