c150624da3c0746bf0ea680e7d437f410be86823
NirmouMounir <mounir.abakhour@gmail.com>
2024-04-22 00:14:38 +0200

    link to problem in comment

---
+++ easy/pb1581.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+
+select v.customer_id, count(v.visit_id) as count_no_trans from Visits v left join Transactions t on v.visit_id = t.visit_id where t.transaction_id IS NULL group by v.customer_id;
+
+select v.customer_id, count(v.visit_id) as count_no_trans from Visits v where not exists (select 1 from Transactions t where v.visit_id = t.visit_id) group by v.customer_id;
